1a11306f5b5b65275ad673ef809c1cb3221c7672
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-07-11 22:34:02 +0300

    Display color codes in horse card components & add minor styling fix for horses greeting

--- src/components/HorseCard.vue
+++ src/components/HorseCard.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="flex flex-col gap-2 bg-slate-600 p-4 rounded-lg shadow-inner shadow-slate-400 m-2">
+  <div class="flex flex-col gap-2 bg-slate-600 p-4 rounded-lg shadow-xl m-2">
     <p>Name: {{ name }}</p>
     <p>Condition: {{ condition }}</p>
-    <p>Color: {{ color }}</p>
+    <div class="flex items-center gap-4">
+      <p>Color: {{ colorName }}</p>
+      <div class="w-5 h-5 rounded-md animate-spin" :style="{ 'background-color': colorCode }"></div>
+    </div>
   </div>
 </template>
 
 <script setup>
-defineProps(['name', 'condition', 'color'])
+defineProps(['name', 'condition', 'colorName', 'colorCode'])
 </script>
+div
--- src/components/HorsesGreeting.vue
+++ src/components/HorsesGreeting.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex p-4 text-lg sm:text-2xl justify-center gap-2">
+  <div class="flex p-4 text-lg sm:text-2xl gap-2">
     <p>Welcome to the horses!</p>
     <p class="sm:animate-bounce">🐴</p>
   </div>
