80b54d37d9fa9879c29b41693654a3403dbe29b3
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-07-11 16:29:45 +0300

    Implement a horse card component in order to show horse informations in horses page

---
+++ src/components/HorseCard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-col gap-2 bg-slate-600 p-4 rounded-lg shadow-inner shadow-slate-400 m-2">
+    <p>Name: {{ name }}</p>
+    <p>Condition: {{ condition }}</p>
+    <p>Color: {{ color }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps(['name', 'condition', 'color'])
+</script>
