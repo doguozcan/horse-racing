d6768b3ced6b138ebcd5316f971c83793bc56442
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-07-11 13:34:32 +0300

    Initialize project

---
+++ src/components/TheAbout.vue
@@ -0,0 +1,5 @@
+<template>
+  <p>about the horse racing</p>
+</template>
+
+<script setup></script>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <p>welcome to the horse racing</p>
+  </div>
+</template>
+
+<script setup></script>
