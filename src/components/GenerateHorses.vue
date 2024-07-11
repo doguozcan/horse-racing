be7f08c0ecf95ae7bad3980fb7033697cbe10748
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-07-11 22:32:19 +0300

    Create generate horses component & add it to horses view

---
+++ src/components/GenerateHorses.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="bg-slate-500 p-4 rounded-lg text-center">
+    <h1>No horse at the moment!</h1>
+    <p>Do you want to generate right now?</p>
+  </div>
+  <ButtonComponent @click="$store.commit('generateHorses')">Yes</ButtonComponent>
+</template>
+
+<script setup>
+import ButtonComponent from '../components/ButtonComponent.vue'
+</script>
--- src/views/HorsesView.vue
+++ src/views/HorsesView.vue
@@ -1,9 +1,13 @@
 <template>
-  <HorsesGreeting />
-  <HorseGrid />
+  <div class="flex flex-col items-center">
+    <HorsesGreeting />
+    <HorseGrid v-if="$store.getters.horsesLength > 0" />
+    <GenerateHorses v-if="$store.getters.horsesLength === 0" />
+  </div>
 </template>
 
 <script setup>
 import HorsesGreeting from '../components/HorsesGreeting.vue'
 import HorseGrid from '../components/HorseGrid.vue'
+import GenerateHorses from '../components/GenerateHorses.vue'
 </script>
