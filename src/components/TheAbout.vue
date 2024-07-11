e773333505dc745f1625efbbe97f24a8cad0fb79
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-07-11 14:53:14 +0300

    Remove temporary views and components

--- src/components/TheAbout.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <p>about the horse racing</p>
-</template>
-
-<script setup></script>
--- src/components/TheWelcome.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <p class="text-5xl text-red-500">welcome to the horse racing</p>
-  </div>
-</template>
-
-<script setup></script>
