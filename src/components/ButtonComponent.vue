60fd0866b4d92beae9a414bbcaf251cefdfc2407
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-07-11 22:30:53 +0300

    Create button component

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,8 @@
+<template>
+  <button
+    type="button"
+    class="bg-green-500 text-white hover:bg-green-800 active:bg-green-400 px-4 py-2 rounded-full my-2 shadow-md shadow-green-200"
+  >
+    <slot></slot>
+  </button>
+</template>
