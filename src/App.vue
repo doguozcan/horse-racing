d51533119fb9167c958687e08d98ccc4bf0bda93
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-07-12 23:11:52 +0300

    Change the style of the app component & explain with detailed comments

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="flex flex-col min-h-screen bg-slate-950 text-slate-100 font-montserrat">
+  <!-- Take up the full screen with a dark slate color using the montserrat font and white text color -->
+  <div class="min-h-screen bg-slate-950 text-white font-montserrat">
     <NavBar />
     <RouterView />
   </div>
 </template>
 
 <script setup>
-import NavBar from './components/NavBar.vue'
+// import the navigation bar that contains horses, programs, races and results views
+import NavBar from './components/common/NavBar.vue'
 </script>
