65d7eb047f2cdfaf3b7a2d2a3b13a83b98c438a3
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-07-12 12:49:29 +0300

    Divide programs into seperate components

--- src/components/RaceProgram.vue
+++ src/components/RaceProgram.vue
@@ -1,21 +1,11 @@
 <template>
   <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
     <div v-for="(program, index) in $store.state.program" :key="index">
-      <div class="border p-4 rounded-lg">
-        <p class="text-xl">Program {{ index + 1 }} - {{ program.length }}m</p>
-        <HorsePosition
-          v-for="(horse, index) in program.horses"
-          :key="horse.name"
-          :name="horse.name"
-          :position="index + 1"
-        >
-        </HorsePosition>
-      </div>
+      <RoundProgram :program="program" :index="index" />
     </div>
   </div>
 </template>
 
 <script setup>
-import HorsePosition from './HorsePosition.vue'
-defineProps(['length', 'horses'])
+import RoundProgram from './RoundProgram.vue'
 </script>
---
+++ src/components/RoundProgram.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="border p-4 rounded-lg">
+    <p class="text-xl">Program {{ index + 1 }} - {{ program.length }}m</p>
+    <HorsePosition
+      v-for="(horse, index) in program.horses"
+      :key="horse.name"
+      :name="horse.name"
+      :position="index + 1"
+    >
+    </HorsePosition>
+  </div>
+</template>
+
+<script setup>
+import HorsePosition from './HorsePosition.vue'
+defineProps(['program', 'index'])
+</script>
