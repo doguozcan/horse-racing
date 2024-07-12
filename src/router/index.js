d81ac739b693665fd8794281269023d561a054ab
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-07-12 12:47:35 +0300

    Add dynamic titles that changes according to the route name

--- src/router/index.js
+++ src/router/index.js
@@ -5,13 +5,20 @@ import Program from '../views/ProgramView.vue'
 import Results from '../views/ResultsView.vue'
 
 const routes = [
-  { path: '/', name: 'Horses', component: Horses },
-  { path: '/races', name: 'Races', component: Races },
-  { path: '/program', name: 'Program', component: Program },
-  { path: '/results', name: 'Results', component: Results }
+  { path: '/', name: 'Horses', component: Horses, meta: { title: 'Horses' } },
+  { path: '/races', name: 'Races', component: Races, meta: { title: 'Races' } },
+  { path: '/program', name: 'Program', component: Program, meta: { title: 'Program' } },
+  { path: '/results', name: 'Results', component: Results, meta: { title: 'Results' } }
 ]
 
-export default createRouter({
+const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title || 'Horse Racing'
+  next()
+})
+
+export default router
