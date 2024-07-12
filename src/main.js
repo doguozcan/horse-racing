934dab02279e0ddc283ce69b1d81e98f27735a78
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-07-12 23:03:39 +0300

    Explain main.js file with detailed comments

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
+// get styles
 import './assets/index.css'
+// create vue app
 import { createApp } from 'vue'
+// import app component
 import App from './App.vue'
+// use Vue Router - reference: https://router.vuejs.org/
 import router from './router'
+// use Vuex - reference: https://vuex.vuejs.org/
 import store from './store'
-
+// initiate app
 const app = createApp(App)
+// connect store to the app
 app.use(store)
+// use router in the app
 app.use(router)
+// mount on index.html's div tag named #app
 app.mount('#app')
