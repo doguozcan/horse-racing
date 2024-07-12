cc8fed457d5b45e4b38c1105f058e117f0636d8e
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-07-12 08:42:57 +0300

    If there are no horses in the race view, create them. If there are no horses or programs in the race view, create both.

--- src/views/ProgramAndResultsView.vue
+++ src/views/ProgramAndResultsView.vue
@@ -2,7 +2,10 @@
   <div class="flex flex-col items-center">
     <GreetUser>Welcome to the Program & Results!</GreetUser>
     <RaceProgram v-if="$store.getters.programLength > 0" />
-    <GenerateProgram v-if="$store.getters.programLength === 0" />
+    <GenerateProgram
+      v-if="$store.getters.horsesLength !== 0 && $store.getters.programLength === 0"
+    />
+    <GenerateHorses v-if="$store.getters.horsesLength === 0" />
   </div>
 </template>
 
@@ -10,4 +13,5 @@
 import GreetUser from '../components/GreetUser.vue'
 import RaceProgram from '../components/RaceProgram.vue'
 import GenerateProgram from '../components/GenerateProgram.vue'
+import GenerateHorses from '../components/GenerateHorses.vue'
 </script>
--- src/views/RacesView.vue
+++ src/views/RacesView.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="flex flex-col items-center">
     <GreetUser>Welcome to the Races!</GreetUser>
+    <GenerateHorses v-if="$store.getters.horsesLength === 0" />
+    <GenerateProgram
+      v-if="$store.getters.horsesLength !== 0 && $store.getters.programLength === 0"
+    />
   </div>
 </template>
 
 <script setup>
 import GreetUser from '../components/GreetUser.vue'
+import GenerateHorses from '../components/GenerateHorses.vue'
+import GenerateProgram from '../components/GenerateProgram.vue'
 </script>
