2228ec6237a6a2637db0fc6cbd2d02434b9c64a6
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-07-12 07:36:53 +0300

    Implement generate program component

---
+++ src/components/GenerateProgram.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="bg-slate-500 p-4 rounded-lg text-center">
+    <h1>No program at the moment!</h1>
+    <p>Do you want to generate right now?</p>
+  </div>
+  <ButtonComponent @click="$store.commit('generateProgram')">Yes</ButtonComponent>
+</template>
+
+<script setup>
+import ButtonComponent from '../components/ButtonComponent.vue'
+</script>
