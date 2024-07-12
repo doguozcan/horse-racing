d9758e2d6283d66694e5cce988178ea0eae22078
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-07-12 23:24:16 +0300

    Change NavBar components style & explained with detailed comment

--- src/components/common/NavBar.vue
+++ src/components/common/NavBar.vue
@@ -1,25 +1,20 @@
 <template>
-  <nav class="flex flex-col sm:flex-row justify-center bg-slate-900">
+  <!-- display the navbar side by side on larger screens, top to bottom on smaller screens -->
+  <nav class="flex flex-col mx-10 sm:flex-row justify-center">
+    <!-- loop through navbar items to create navigation links -->
     <RouterLink
       v-for="(navBarItem, navBarIndex) in navBarItems"
       :key="navBarIndex"
       :to="navBarItem.path"
     >
-      <div
-        class="m-1 py-1 rounded-lg text-center sm:m-4 sm:px-4 sm:py-2 hover:opacity-90 active:opacity-100"
-        :class="{
-          'bg-slate-800': $route.path !== navBarItem.path,
-          'bg-slate-400 shadow-md shadow-slate-500': $route.path === navBarItem.path
-        }"
-      >
-        {{ navBarItem.label }}
-      </div>
+      <NavBarItem :navBarItem="navBarItem" />
     </RouterLink>
   </nav>
 </template>
 
 <script setup>
 import { RouterLink } from 'vue-router'
+import NavBarItem from './NavBarItem.vue'
 
 const navBarItems = [
   { path: '/', label: 'Horses' },
