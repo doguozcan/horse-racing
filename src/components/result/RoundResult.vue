cf84f7f3a58e0a34928046aa9ecc23827bdb18f8
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-07-13 23:35:21 +0300

    Improve round results UI and transfer that component to components/result sub-folder

---
+++ src/components/result/RoundResult.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-col">
+    <H1Tag class="text-center">Round {{ index + 1 }}</H1Tag>
+    <div class="bg-slate-900 rounded-lg m-2">
+      <ResultHorse
+        v-for="(horseInformation, horseIndex) in result"
+        :key="horseIndex"
+        :horseInformation="horseInformation"
+        :horseIndex="horseIndex"
+        :raceIndex="index"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import H1Tag from '../common/H1Tag.vue'
+import ResultHorse from '../ResultHorse.vue'
+
+defineProps(['result', 'index'])
+</script>
