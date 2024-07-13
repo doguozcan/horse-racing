30e6eb1034f158b79dcba97786f214262cc51b57
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-07-13 18:20:29 +0300

    Improve round program component's styling & re-import program table in program view with the current component name

--- src/components/RoundProgram.vue
+++ src/components/RoundProgram.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="border p-4 rounded-lg">
-    <p class="text-xl">Program {{ index + 1 }} - {{ program.length }}m</p>
+  <!-- display round's distance information and list of horses in the round -->
+  <div class="bg-slate-800 p-4 rounded-lg m-6 sm:m-4 md:m-2">
+    <!-- title of the round -->
+    <H1Tag class="text-center">Program {{ index + 1 }} - {{ program.length }}m 🏁</H1Tag>
+    <!-- display each horse dynamically -->
     <HorsePosition
       v-for="(horse, index) in program.horses"
       :key="horse.name"
@@ -12,6 +15,8 @@
 </template>
 
 <script setup>
+import H1Tag from './common/H1Tag.vue'
 import HorsePosition from './HorsePosition.vue'
+
 defineProps(['program', 'index'])
 </script>
--- src/views/ProgramView.vue
+++ src/views/ProgramView.vue
@@ -1,11 +1,11 @@
 <template>
   <GenerateHorses />
   <GenerateProgram />
-  <RaceProgram v-if="$store.getters.programLength > 0" />
+  <ProgramTable />
 </template>
 
 <script setup>
 import GenerateHorses from '../components/horse/GenerateHorses.vue'
 import GenerateProgram from '../components/program/GenerateProgram.vue'
-import RaceProgram from '../components/RaceProgram.vue'
+import ProgramTable from '../components/program/ProgramTable.vue'
 </script>
