fd4c78e299759dc251ebf515563507af2e207b75
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-07-13 23:43:27 +0300

    Implement a horse ranking component in order to display the place of the horse at the end of the round with corresponding medal color which are golden, silver and bronze

---
+++ src/components/result/HorseRanking.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="flex items-center justify-center w-8 h-8 sm:w-10 sm:h-10 rounded-full font-bold"
+    :class="{
+      'bg-gold text-black': horseIndex === 0,
+      'bg-silver text-black': horseIndex === 1,
+      'bg-bronze text-black': horseIndex === 2
+    }"
+  >
+    <p>{{ horseIndex + 1 }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps(['horseIndex'])
+</script>
