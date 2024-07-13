9562bc9692811d116bc4ddaeddeaffbf46652185
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-07-13 18:10:57 +0300

    Improve program table's styling

---
+++ src/components/program/ProgramTable.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- display only when program is available -->
+  <div
+    class="grid w-full grid-cols-1 sm:grid-cols-2 md:grid-cols-3"
+    v-if="$store.getters.programLength > 0"
+  >
+    <!-- display each program obtained from the store -->
+    <div v-for="(program, index) in $store.state.program" :key="index">
+      <RoundProgram :program="program" :index="index" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import RoundProgram from '../RoundProgram.vue'
+</script>
