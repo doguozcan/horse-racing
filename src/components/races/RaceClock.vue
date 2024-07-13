aaedf47dfba0ebe54a3f4d625e24494dc8786545
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-07-14 00:40:24 +0300

    Display the time using an animated emoji clock & add race information component to the race view

---
+++ src/components/races/RaceClock.vue
@@ -0,0 +1,9 @@
+<template>
+  <p class="text-3xl sm:text-4xl md:text-5xl">
+    {{ clocks[$store.state.currentTime % 12] }}
+  </p>
+</template>
+
+<script setup>
+const clocks = ['🕐', '🕑', '🕒', '🕓', '🕔', '🕕', '🕖', '🕗', '🕘', '🕙', '🕚', '🕛']
+</script>
--- src/views/RacesView.vue
+++ src/views/RacesView.vue
@@ -2,12 +2,14 @@
   <GenerateHorses />
   <GenerateProgram />
   <StartRace />
-  <RaceTrack v-if="$store.getters.programLength !== 0 && 1 == 2" />
+  <RaceInformation />
+  <RaceTrack />
 </template>
 
 <script setup>
 import GenerateHorses from '../components/horse/GenerateHorses.vue'
 import GenerateProgram from '../components/program/GenerateProgram.vue'
 import StartRace from '../components/races/StartRace.vue'
-import RaceTrack from '../components/RaceTrack.vue'
+import RaceInformation from '../components/races/RaceInformation.vue'
+import RaceTrack from '../components/races/RaceTrack.vue'
 </script>
