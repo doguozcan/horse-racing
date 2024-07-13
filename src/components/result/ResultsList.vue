383088167dd5aadb8064e8977efa5e9cab506276
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-07-13 23:04:14 +0300

    Improve styling of the results list component & navigate results component into components/result sub-folder

---
+++ src/components/result/ResultsList.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- display grid when the first round is completed -->
+  <div class="w-full grid grid-cols-1 sm:grid-cols-2" v-if="$store.getters.resultsLength !== 0">
+    <!-- display leaderboards for every single round -->
+    <RoundResult
+      v-for="(result, index) in $store.state.results"
+      :key="index"
+      :result="result"
+      :index="index"
+    />
+  </div>
+</template>
+
+<script setup>
+import RoundResult from './RoundResult.vue'
+</script>
--- src/views/ResultsView.vue
+++ src/views/ResultsView.vue
@@ -2,12 +2,12 @@
   <GenerateProgram />
   <GenerateHorses />
   <NoRoundCompleted />
-  <ResultsList v-if="$store.getters.resultsLength !== 0" />
+  <ResultsList />
 </template>
 
 <script setup>
 import GenerateProgram from '../components/program/GenerateProgram.vue'
 import GenerateHorses from '../components/horse/GenerateHorses.vue'
 import NoRoundCompleted from '../components/result/NoRoundCompleted.vue'
-import ResultsList from '../components/ResultsList.vue'
+import ResultsList from '../components/result/ResultsList.vue'
 </script>
