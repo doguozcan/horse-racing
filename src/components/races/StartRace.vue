293357e9dce051b9a45621adf9caecd0415992fb
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-07-13 23:24:26 +0300

    Improve style of the results list & add descriptive messages for users in start race component

--- src/components/races/StartRace.vue
+++ src/components/races/StartRace.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="flex flex-col items-center" v-if="$store.getters.programLength !== 0">
-    <H1Tag>Good luck to all jockeys and horses! 🍀</H1Tag>
+    <H1Tag v-if="$store.getters.resultsLength !== 6">Good luck to all jockeys and horses! 🍀</H1Tag>
+    <H1Tag v-if="$store.getters.resultsLength === 6"
+      >Congratulations to every single jockey and horse! 🎉</H1Tag
+    >
+    <H1Tag v-if="$store.getters.resultsLength === 6">
+      You can navigate to the
+      <RouterLink
+        to="/results"
+        class="text-slate-400 hover:text-slate-300 duration-100 font-bold animate-pulse"
+        >results</RouterLink
+      >
+      page to see the leaderboard for every single round!</H1Tag
+    >
     <!-- start button is disabled when race is already running -->
     <div class="flex flex-row gap-2 sm:gap-4 mt-2 sm:mt-4">
       <ButtonComponent
--- src/components/result/ResultsList.vue
+++ src/components/result/ResultsList.vue
@@ -1,6 +1,9 @@
 <template>
   <!-- display grid when the first round is completed -->
-  <div class="w-full grid grid-cols-1 sm:grid-cols-2" v-if="$store.getters.resultsLength !== 0">
+  <div
+    class="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3"
+    v-if="$store.getters.resultsLength !== 0"
+  >
     <!-- display leaderboards for every single round -->
     <RoundResult
       v-for="(result, index) in $store.state.results"
