f0e8397156fb608a89516915f436a5aafb07880a
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-07-18 15:53:30 +0300

    Make horse condition emojis more detailed

--- src/components/horse/HorseCondition.vue
+++ src/components/horse/HorseCondition.vue
@@ -1,6 +1,31 @@
 <template>
   <!-- displaying the condition of the horse with an emoji -->
-  <PTag>Condition: {{ condition }} {{ condition > 50 ? '🔋' : '🪫' }}</PTag>
+  <PTag
+    >Condition: {{ condition }}
+    {{
+      condition === 100
+        ? '🔟'
+        : condition > 90
+          ? '9️⃣'
+          : condition > 80
+            ? '8️⃣'
+            : condition > 70
+              ? '7️⃣'
+              : condition > 60
+                ? '6️⃣'
+                : condition > 50
+                  ? '5️⃣'
+                  : condition > 40
+                    ? '4️⃣'
+                    : condition > 30
+                      ? '3️⃣'
+                      : condition > 20
+                        ? '2️⃣'
+                        : condition > 10
+                          ? '1️⃣'
+                          : '0️⃣'
+    }}</PTag
+  >
 </template>
 
 <script setup>
